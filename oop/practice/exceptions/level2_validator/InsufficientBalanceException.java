36ccb6535a92c01a78fccb60ffbaf2a4ac0a7158
Pratik Priyam <pratik.priyam.iitian@gmail.com>
2026-05-31 09:54:02 +0530

    Fix InsufficientBalanceException to correctly instantiate parent message and enforce separation of concerns

--- oop/practice/exceptions/level2_validator/InsufficientBalanceException.java
+++ oop/practice/exceptions/level2_validator/InsufficientBalanceException.java
@@ -2,6 +2,6 @@ package oop.practice.exceptions.level2_validator;
 
 public class InsufficientBalanceException extends Exception{
     public InsufficientBalanceException(String message){
-        System.err.println(message);
+        super(message);
     }
 }
